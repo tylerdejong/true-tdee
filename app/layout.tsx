18d9996d75a7ff3c99e2cab73e17691ca1d9504c
Tyler De Jong <tdj@Tylers-MacBook-Pro.local>
2026-06-23 11:02:05 +1000

    added google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,6 +47,21 @@ export const viewport: Viewport = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-9B3YYTTX48"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-9B3YYTTX48');
+`
+          }}
+        />
+      </head>
       <body>
         {adsEnabled && (
           <Script
